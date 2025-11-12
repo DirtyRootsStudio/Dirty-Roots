319ad554fcbd6c01e442f97205fe50224cf0fe9e
Pepe Ortiz Roldán <99665744+madgIitch@users.noreply.github.com>
2025-11-12 14:16:23 +0100

    aver

--- pwa/src/types/place.ts
+++ pwa/src/types/place.ts
@@ -1,17 +1,20 @@
 // src/types/place.ts  
 import { Timestamp } from "firebase/firestore";  
   
-export interface Place {  
-  id?: string;  
-  name: string;  
-  city?: string;  
-  description?: string;  
-  photo?: string | null;  
-  coords: { lat: number; lng: number };  
-  geohash: string;  
-  tags?: string[];  
-  noiseLevel?: number;  
-  createdBy: string;  
-  createdAt: Timestamp;  
-  status?: "approved" | "pending";  
-}
+export type Place = {    
+  id?: string;    
+  name: string;    
+  city: string;    
+  placeType?: "park" | "cafe"; // NUEVO  
+  address?: string; // NUEVO  
+  schedule?: string; // NUEVO  
+  description?: string;    
+  photo?: string | null;    
+  coords: { lat: number; lng: number };    
+  geohash: string;    
+  tags?: string[];    
+  // noiseLevel?: number; // ELIMINADO  
+  createdBy: string;    
+  createdAt: Timestamp;    
+  status?: "approved" | "pending";    
+};
